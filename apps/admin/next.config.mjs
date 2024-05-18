a8994ecff684ad5d0d646579d8aa69f5d2c8f4b1
jaehoo <im.jaehoo1@gmail.com>
2024-05-18 14:30:23 +0900

    matchmaker

--- apps/admin/next.config.mjs
+++ apps/admin/next.config.mjs
@@ -16,6 +16,16 @@ const config = {
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+  images: {
+    remotePatterns: [
+      {
+        hostname: process.env.NEXT_PUBLIC_SUPABASE_HOSTNAME ?? "",
+      },
+    ],
+  },
+  experimental: {
+    scrollRestoration: true,
+  },
 };
 
 export default config;
