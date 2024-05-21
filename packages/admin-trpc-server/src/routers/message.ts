b2ab7809404027d732d4ba522f6024e7a0be44ef
jaehoo <im.jaehoo1@gmail.com>
2024-05-21 14:37:20 +0900

    subject

--- packages/admin-trpc-server/src/routers/message.ts
+++ packages/admin-trpc-server/src/routers/message.ts
@@ -38,13 +38,15 @@ export const adminMessageRouter = createTRPCRouter({
       return solapiMessageService.send({
         to: member.phoneNumber,
         from: process.env.ADMIN_PHONE_NUMBER,
-        subject: "[내편소] 매칭 제안드려요!",
+        subject: "[내편소] 매칭 안내",
         text: `${member.name} 님 안녕하세요! 내편소 호스트입니다. 매칭 제안드려요.
 
-아래 사이트에서 상대방 프로필을 확인하고 수락 여부를 결정해주세요.
+아래 사이트에서 상대방 프로필을 확인하시고 수락 여부를 결정해주세요.
 ${process.env.NEXT_PUBLIC_MATCHMAKER_DOMAIN_HOSTNAME}
 
-양쪽 모두 수락하시면 소개가 성사되며 호스트가 연락을 드릴게요. 감사합니다 :)
+양쪽 모두 수락하시면 소개가 성사되며 호스트가 연락을 드립니다.
+
+감사합니다!
 
 (개인정보를 우려하시는 분들이 많아 시스템을 바꿔봤습니다. 혹시 버그가 있다면 호스트에게 제보해주세요!)
         `,
