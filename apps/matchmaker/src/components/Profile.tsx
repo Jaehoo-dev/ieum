c6017ca260f53ae60009c7cfcae7fa3b1a38a83a
jaehoo <im.jaehoo1@gmail.com>
2024-05-21 14:48:09 +0900

    update

--- apps/matchmaker/src/components/Profile.tsx
+++ apps/matchmaker/src/components/Profile.tsx
@@ -36,8 +36,8 @@ export function Profile({ profile, watermarkText }: Props) {
       <div className="flex w-full items-start gap-1">
         <p className="text-md text-gray-800">※</p>
         <p className="text-md text-gray-800">
-          스크린 캡처 및 무단 유출을 절대 금지합니다. 무단 유출 시 법적 책임을
-          질 수 있습니다.
+          스크린 캡처 및 무단 유출을 절대 금지합니다. 무단 유출 시 로그와
+          워터마크로 추적이 가능하며 법적 책임을 질 수 있습니다.
         </p>
       </div>
       <div className="flex w-full flex-col gap-2 rounded-lg border-2 border-primary-500 p-3">
