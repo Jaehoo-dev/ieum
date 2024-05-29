167371b2f3c329a91004e7a26bec2f994eb94161
jaehoo <im.jaehoo1@gmail.com>
2024-05-29 17:58:52 +0900

    log

--- apps/matchmaker/src/components/BlogLink.tsx
+++ apps/matchmaker/src/components/BlogLink.tsx
@@ -1,15 +1,22 @@
 import type { ComponentProps } from "react";
 import Link from "next/link";
 
+import { useSlackNotibot } from "~/hooks/useSlackNotibot";
+
 type Props = Omit<ComponentProps<typeof Link>, "href">;
 
 export function BlogLink({ target, rel, ...props }: Props) {
+  const { sendMessage } = useSlackNotibot();
+
   return (
     <Link
       href={DEFAULT_BLOG_URL}
       className="text-lg text-gray-500 underline hover:text-gray-700"
       target={target ?? "_blank"}
       rel={rel ?? "noopener noreferrer"}
+      onClick={() => {
+        sendMessage("'소개팅 꿀팁 보러 가기' 클릭");
+      }}
       {...props}
     >
       소개팅 꿀팁 보러 가기
