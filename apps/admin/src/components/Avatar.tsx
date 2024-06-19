f90c56135c80597fa0b0965b873e34431e9457bc
jaehoo <im.jaehoo1@gmail.com>
2024-06-20 04:03:04 +0900

    size

--- apps/admin/src/components/Avatar.tsx
+++ apps/admin/src/components/Avatar.tsx
@@ -1,7 +1,7 @@
 import { MemberImage } from "@ieum/prisma";
 import { supabase } from "@ieum/supabase";
 
-const 너비 = 100;
+const 크기 = 100;
 
 export function Avatar({ image }: { image: MemberImage }) {
   const {
@@ -10,10 +10,17 @@ export function Avatar({ image }: { image: MemberImage }) {
     .from(process.env.NEXT_PUBLIC_SUPABASE_BASIC_MEMBER_IMAGES_BUCKET_NAME!)
     .getPublicUrl(image.bucketPath, {
       transform: {
-        width: 너비,
-        resize: "contain",
+        width: 크기,
+        height: 크기,
       },
     });
 
-  return <img src={publicUrl} alt="프로필 사진" width={너비} />;
+  return (
+    <img
+      src={publicUrl}
+      alt="프로필 사진"
+      width={크기}
+      className="rounded-full"
+    />
+  );
 }
