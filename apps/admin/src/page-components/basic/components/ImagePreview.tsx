6d7d2deb1467e58264a04e30dfb80326944d0ed1
jaehoo <im.jaehoo1@gmail.com>
2024-06-21 13:28:37 +0900

    update

--- apps/admin/src/page-components/basic/components/ImagePreview.tsx
+++ apps/admin/src/page-components/basic/components/ImagePreview.tsx
@@ -5,12 +5,7 @@ export function ImagePreview({ bucketPath }: { bucketPath: string }) {
     data: { publicUrl },
   } = supabase.storage
     .from(process.env.NEXT_PUBLIC_SUPABASE_BASIC_MEMBER_IMAGES_BUCKET_NAME!)
-    .getPublicUrl(bucketPath, {
-      transform: {
-        width: 200,
-        resize: "contain",
-      },
-    });
+    .getPublicUrl(bucketPath);
 
-  return <img src={publicUrl} alt="사진" width={200} height={200} />;
+  return <img src={publicUrl} alt="사진" width={400} />;
 }
