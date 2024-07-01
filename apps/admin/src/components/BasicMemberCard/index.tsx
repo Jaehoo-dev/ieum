e4b88bfc43f8b93f798c35162400a3503e938472
jaehoo <im.jaehoo1@gmail.com>
2024-07-01 15:28:13 +0900

    update

--- apps/admin/src/components/BasicMemberCard/index.tsx
+++ apps/admin/src/components/BasicMemberCard/index.tsx
@@ -30,53 +30,45 @@ export function BasicMemberCard({ member, defaultMode }: Props) {
   ]);
 
   return (
-    <div className="w-full max-w-3xl rounded-lg border border-gray-200 p-4">
+    <div className="flex w-full max-w-3xl flex-col gap-2 rounded-lg border border-gray-200 p-4">
       <div className="flex items-center justify-between">
         <div className="flex flex-col gap-1">
           <div className="flex items-center gap-2">
             {member.images[0] != null ? (
               <Avatar image={member.images[0]} style={{ marginRight: "4px" }} />
             ) : null}
-            <Link
-              href={`/basic/members/${member.id}/update`}
-              className={`${
-                member.status !== MemberStatus.ACTIVE
-                  ? "text-gray-400"
-                  : member.gender === Gender.MALE
-                  ? "text-blue-500"
-                  : "text-pink-500"
-              } text-lg font-semibold`}
-            >
-              {member.name}
-            </Link>
-            <span>|</span>
-            {member.profile == null ? (
+            <div className="flex flex-col">
               <Link
-                href={`/basic/members/${member.id}/profile/create`}
-                className="text-blue-600 hover:underline"
+                href={`/basic/members/${member.id}/update`}
+                className={`${
+                  member.status !== MemberStatus.ACTIVE
+                    ? "text-gray-400"
+                    : member.gender === Gender.MALE
+                    ? "text-blue-500"
+                    : "text-pink-500"
+                } text-lg font-semibold`}
               >
-                프로필 생성
+                {`${member.name}(${member.phoneNumber.slice(-4)})`}
               </Link>
-            ) : (
-              <Link
-                href={`/basic/members/${member.id}/profile`}
-                target="_blank"
-                rel="noreferrer noopener"
-                className="text-blue-600 hover:underline"
-              >
-                프로필 보기
-              </Link>
-            )}
+              {member.profile == null ? (
+                <Link
+                  href={`/basic/members/${member.id}/profile/create`}
+                  className="text-blue-600 hover:underline"
+                >
+                  프로필 생성
+                </Link>
+              ) : (
+                <Link
+                  href={`/basic/members/${member.id}/profile`}
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  className="text-blue-600 hover:underline"
+                >
+                  프로필 보기
+                </Link>
+              )}
+            </div>
           </div>
-          <span>{`백로그 ${
-            matchesCountByStatus[MatchStatus.BACKLOG]
-          } / 준비중 ${matchesCountByStatus[MatchStatus.PREPARING]} / 진행중 ${
-            matchesCountByStatus[MatchStatus.PENDING] +
-            matchesCountByStatus[MatchStatus.ACCEPTED]
-          } / 완료 ${
-            matchesCountByStatus[MatchStatus.REJECTED] +
-            matchesCountByStatus[MatchStatus.BROKEN_UP]
-          }`}</span>
         </div>
         <div className="flex flex-col items-end gap-1">
           <div className="flex items-center gap-2">
@@ -121,9 +113,18 @@ export function BasicMemberCard({ member, defaultMode }: Props) {
           </div>
         </div>
       </div>
+      <span>{`백로그 ${matchesCountByStatus[MatchStatus.BACKLOG]} / 준비중 ${
+        matchesCountByStatus[MatchStatus.PREPARING]
+      } / 진행중 ${
+        matchesCountByStatus[MatchStatus.PENDING] +
+        matchesCountByStatus[MatchStatus.ACCEPTED]
+      } / 완료 ${
+        matchesCountByStatus[MatchStatus.REJECTED] +
+        matchesCountByStatus[MatchStatus.BROKEN_UP]
+      }`}</span>
       {folded ? null : (
         <div className="flex flex-col gap-2">
-          <div className="mt-2 flex gap-4">
+          <div className="flex gap-4">
             {mode === "DETAILED" ? (
               <DetailedSelfFields member={member} />
             ) : (
