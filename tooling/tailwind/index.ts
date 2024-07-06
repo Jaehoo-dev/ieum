bfad291cc3a18abfef58670f25be31f516d7853f
jaehoo <im.jaehoo1@gmail.com>
2024-07-06 19:15:07 +0900

    sidebar

--- tooling/tailwind/index.ts
+++ tooling/tailwind/index.ts
@@ -10,6 +10,7 @@ export default {
       },
       colors: {
         "primary-100": "#ffefe8",
+        "primary-200": "#ffdccf",
         "primary-300": "#ffcfba",
         "primary-500": "#ff8271",
         "primary-600": "#ff725c",
