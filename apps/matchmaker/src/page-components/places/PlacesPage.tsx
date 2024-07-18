63e4bf80a5576a22ad2cdfedafccf21d8738045c
jaehoo <im.jaehoo1@gmail.com>
2024-07-18 13:09:06 +0900

    change ads layout

--- apps/matchmaker/src/page-components/places/PlacesPage.tsx
+++ apps/matchmaker/src/page-components/places/PlacesPage.tsx
@@ -49,6 +49,7 @@ export function PlacesPage({ places }: Props) {
       </Head>
       <div className="flex flex-col gap-6 px-2">
         <div className="mb-24 flex flex-col gap-8">
+          <HorizontalInfeedAd />
           {Object.entries(places).map(([district, places]) => {
             return (
               <section key={district} className="flex flex-col gap-2">
@@ -86,7 +87,6 @@ export function PlacesPage({ places }: Props) {
                     );
                   })}
                 </ul>
-                <HorizontalInfeedAd />
               </section>
             );
           })}
