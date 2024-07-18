50ae931f4c2c709a6b68aa22c5a2f035e2dfc274
jaehoo <im.jaehoo1@gmail.com>
2024-07-19 01:33:02 +0900

    use ResponsiveDisplayAd

--- apps/matchmaker/src/page-components/places/custom-regions/PlacesByCustomRegionPage.tsx
+++ apps/matchmaker/src/page-components/places/custom-regions/PlacesByCustomRegionPage.tsx
@@ -9,7 +9,7 @@ import {
 } from "@ieum/constants";
 import { CuisineType, CustomRegion, Place } from "@ieum/prisma";
 
-import { HorizontalInfeedAd } from "~/components/adsense/HorizontalInfeedAd";
+import { ResponsiveDisplayAd } from "~/components/adsense/ResponsiveDisplayAd";
 import { Layout } from "~/components/Layout";
 import { useSlackNotibot } from "~/hooks/useSlackNotibot";
 
@@ -98,7 +98,7 @@ export function PlacesByCustomRegionPage({ customRegion, places }: Props) {
               </section>
             );
           })}
-          <HorizontalInfeedAd />
+          <ResponsiveDisplayAd />
         </div>
         <div className="fixed bottom-0 left-0 z-30 flex w-full items-center justify-center border-t border-gray-200 bg-white p-4 md:px-6">
           <div className="w-full max-w-lg px-2">
