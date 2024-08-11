dfffb4f66cad9dc4a29b31d7f212b5414f236fe4
jaehoo <im.jaehoo1@gmail.com>
2024-08-11 21:46:51 +0900

    auth

--- apps/admin/src/page-components/Home.tsx
+++ apps/admin/src/page-components/Home.tsx
@@ -1,15 +1,11 @@
-import type { ReactElement } from "react";
-
 import { Layout } from "~/components/Layout";
 
 export function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <h1 className="text-4xl font-semibold">이음 어드민</h1>
-    </div>
+    <Layout>
+      <div className="flex min-h-screen flex-col items-center justify-center py-2">
+        <h1 className="text-4xl font-semibold">이음 어드민</h1>
+      </div>
+    </Layout>
   );
 }
-
-Home.getLayout = function getLayout(page: ReactElement) {
-  return <Layout>{page}</Layout>;
-};
