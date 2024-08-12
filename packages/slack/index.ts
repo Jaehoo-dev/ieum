5c83aaee9ebdea06cef55888a4e05ad80d94d629
jaehoo <im.jaehoo1@gmail.com>
2024-08-12 16:52:57 +0900

    폼 알림 나눔

--- packages/slack/index.ts
+++ packages/slack/index.ts
@@ -2,7 +2,8 @@ export type 슬랙_채널 =
   | "에러_알림"
   | "알림"
   | "매칭_결과_알림"
-  | "폼_제출_알림";
+  | "폼_제출_알림"
+  | "폼_백업";
 
 export async function sendSlackMessage({
   channel,
@@ -16,6 +17,7 @@ export async function sendSlackMessage({
     알림: process.env.NEXT_PUBLIC_SLACK_NOTIBOT_WEBHOOK_URL!,
     매칭_결과_알림: process.env.NEXT_PUBLIC_SLACK_MATCH_NOTIBOT_WEBHOOK_URL!,
     폼_제출_알림: process.env.NEXT_PUBLIC_SLACK_FORM_NOTIBOT_WEBHOOK_URL!,
+    폼_백업: process.env.NEXT_PUBLIC_SLACK_FORM_BACKUP_WEBHOOK_URL!,
   };
 
   try {
