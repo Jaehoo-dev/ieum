879f24733792043e404a4283746bce7944814172
jaehoo <im.jaehoo1@gmail.com>
2024-08-12 23:09:37 +0900

    update

--- apps/matchmaker/src/page-components/demo/register/components/Ending.tsx
+++ apps/matchmaker/src/page-components/demo/register/components/Ending.tsx
@@ -14,21 +14,19 @@ export function Ending({ targetUrl }: Props) {
       />
       <div className="mt-2 flex w-full flex-col gap-8 p-8">
         <div className="flex flex-col gap-6">
-          <h1 className="text-lg font-medium text-gray-800">
+          <h1 className="text-xl font-medium text-gray-800">
             설문을 완료했습니다.
           </h1>
-          <p className="text-gray-700">
+          <h2 className="text-lg text-gray-700">
             더 꼼꼼한 설문을 기반으로 이상형을 만나보시는 건 어떨까요?
-          </p>
+          </h2>
         </div>
-        <div className="flex flex-col gap-1">
-          <Link
-            href={targetUrl}
-            className="rounded-lg bg-primary-500 py-2 text-center text-lg text-white hover:bg-primary-700"
-          >
-            소개받으러 가기
-          </Link>
-        </div>
+        <Link
+          href={targetUrl}
+          className="rounded-lg bg-primary-500 py-2 text-center text-lg font-medium text-white hover:bg-primary-700"
+        >
+          소개받으러 가기
+        </Link>
       </div>
     </div>
   );
--- apps/matchmaker/src/page-components/demo/register/components/Welcome.tsx
+++ apps/matchmaker/src/page-components/demo/register/components/Welcome.tsx
@@ -10,15 +10,17 @@ export function Welcome({ onNext }: Props) {
         alt="안녕하세요"
         className="h-2/5 w-full object-cover object-center"
       />
-      <div className="mt-2 flex w-full flex-col gap-6 p-8">
-        <h1 className="text-xl font-medium text-gray-800">
-          안녕하세요! 이 설문은 이음💘 설문 체험판으로, 실제 설문의 일부입니다.
-        </h1>
-        <h2 className="text-lg text-gray-700">
-          개인을 특정할 수 있는 문항은 없으며, 답변을 저장하지도 않으니 편하게
-          둘러보세요. 😊
-        </h2>
-        <div className="flex flex-col gap-8"></div>
+      <div className="mt-2 flex w-full flex-col gap-8 p-8">
+        <div className="flex flex-col gap-6">
+          <h1 className="text-xl font-medium text-gray-800">
+            안녕하세요! 이 설문은 이음💘 설문 체험판으로, 실제 설문의
+            일부입니다.
+          </h1>
+          <h2 className="text-lg text-gray-700">
+            개인을 특정할 수 있는 문항은 없으며, 답변을 저장하지도 않으니 편하게
+            둘러보세요. 😊
+          </h2>
+        </div>
         <button
           onClick={onNext}
           className="w-full rounded-lg bg-primary-500 px-4 py-2 text-lg font-medium text-white hover:bg-primary-700 disabled:cursor-not-allowed disabled:bg-primary-300"
