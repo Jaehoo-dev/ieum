107603aec3e83d4fc8591a0c968ed4f9a14e0238
jaehoo <im.jaehoo1@gmail.com>
2024-08-29 01:49:45 +0900

    fix

--- apps/reviews/src/pages/index.tsx
+++ apps/reviews/src/pages/index.tsx
@@ -7,11 +7,13 @@ import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import { Review } from "~/components/Review";
 import { api } from "~/utils/api";
 
+type OrderBy = "PRIORITY" | "WRITTEN_AT";
+
 export default function Home() {
   const router = useRouter();
   const [page, setPage] = useState(0);
   const [pageGroup, setPageGroup] = useState(0);
-  const [orderBy, setOrderBy] = useState<"PRIORITY" | "WRITTEN_AT">("PRIORITY");
+  const [orderBy, setOrderBy] = useState<OrderBy>("PRIORITY");
   const { data } = api.reviewRouter.getFripReviews.useQuery({
     skip: page * 페이지당_후기_개수,
     take: 페이지당_후기_개수,
@@ -46,88 +48,91 @@ export default function Home() {
     Math.floor(count / 페이지당_후기_개수 / 페이지그룹당_페이지_개수);
 
   return (
-    <div className="flex flex-col gap-6 border-t border-t-gray-500 pt-4">
+    <div className="mt-4 flex flex-col gap-3">
       <div className="flex justify-end">
         <select
-          className="rounded-lg border border-gray-200 p-[10px] text-sm font-bold"
+          className="rounded-lg border border-gray-200 p-2 text-xs font-bold text-[#363636]"
           value={orderBy}
           onChange={(event) => {
-            setOrderBy(event.target.value as "PRIORITY" | "WRITTEN_AT");
+            setOrderBy(event.target.value as OrderBy);
           }}
         >
           <option value="PRIORITY">추천순</option>
           <option value="WRITTEN_AT">최신순</option>
         </select>
       </div>
-      <div className="flex flex-col gap-4">
-        {reviews.map((review) => {
-          return <Review key={review.id} data={review} />;
-        })}
-      </div>
-      <div className="flex items-center justify-center gap-4">
-        <button
-          onClick={() => {
-            const oldPageGroup = pageGroup;
+      <div className="flex flex-col gap-6">
+        <div className="border-b border-b-gray-500" />
+        <div className="flex flex-col gap-4">
+          {reviews.map((review) => {
+            return <Review key={review.id} data={review} />;
+          })}
+        </div>
+        <div className="flex items-center justify-center gap-4">
+          <button
+            onClick={() => {
+              const oldPageGroup = pageGroup;
 
-            setPageGroup(oldPageGroup - 1);
-            setPage(oldPageGroup * 페이지그룹당_페이지_개수 - 1);
+              setPageGroup(oldPageGroup - 1);
+              setPage(oldPageGroup * 페이지그룹당_페이지_개수 - 1);
 
-            sendScrollTriggerToParent({
-              target: iframeParentTarget,
-              device: parentDevice,
-            });
-          }}
-          disabled={!hasPreviousPageGroup}
-        >
-          <ChevronLeftIcon className="pb-0.5 text-gray-400" />
-        </button>
-        {Array.from({
-          length: Math.min(
-            페이지그룹당_페이지_개수,
-            Math.ceil(count / 페이지당_후기_개수) -
-              pageGroup * 페이지그룹당_페이지_개수,
-          ),
-        }).map((_, index) => {
-          const pageNumber = pageGroup * 페이지그룹당_페이지_개수 + index;
-          const isCurrentPage = page === pageNumber;
+              sendScrollTriggerToParent({
+                target: iframeParentTarget,
+                device: parentDevice,
+              });
+            }}
+            disabled={!hasPreviousPageGroup}
+          >
+            <ChevronLeftIcon className="pb-0.5 text-gray-400" />
+          </button>
+          {Array.from({
+            length: Math.min(
+              페이지그룹당_페이지_개수,
+              Math.ceil(count / 페이지당_후기_개수) -
+                pageGroup * 페이지그룹당_페이지_개수,
+            ),
+          }).map((_, index) => {
+            const pageNumber = pageGroup * 페이지그룹당_페이지_개수 + index;
+            const isCurrentPage = page === pageNumber;
 
-          return (
-            <button
-              key={pageNumber}
-              disabled={pageNumber * 페이지당_후기_개수 >= count}
-              onClick={() => {
-                setPage(pageNumber);
-                sendScrollTriggerToParent({
-                  target: iframeParentTarget,
-                  device: parentDevice,
-                });
-              }}
-              className={`${
-                isCurrentPage
-                  ? "font-semibold text-gray-700"
-                  : "text-sm text-gray-400 "
-              }`}
-            >
-              {pageNumber + 1}
-            </button>
-          );
-        })}
-        <button
-          onClick={() => {
-            const newPageGroup = pageGroup + 1;
+            return (
+              <button
+                key={pageNumber}
+                disabled={pageNumber * 페이지당_후기_개수 >= count}
+                onClick={() => {
+                  setPage(pageNumber);
+                  sendScrollTriggerToParent({
+                    target: iframeParentTarget,
+                    device: parentDevice,
+                  });
+                }}
+                className={`${
+                  isCurrentPage
+                    ? "font-semibold text-gray-700"
+                    : "text-sm text-gray-400 "
+                }`}
+              >
+                {pageNumber + 1}
+              </button>
+            );
+          })}
+          <button
+            onClick={() => {
+              const newPageGroup = pageGroup + 1;
 
-            setPageGroup(newPageGroup);
-            setPage(newPageGroup * 페이지그룹당_페이지_개수);
+              setPageGroup(newPageGroup);
+              setPage(newPageGroup * 페이지그룹당_페이지_개수);
 
-            sendScrollTriggerToParent({
-              target: iframeParentTarget,
-              device: parentDevice,
-            });
-          }}
-          disabled={!hasNextPageGroup}
-        >
-          <ChevronRightIcon className="pb-0.5 text-gray-400" />
-        </button>
+              sendScrollTriggerToParent({
+                target: iframeParentTarget,
+                device: parentDevice,
+              });
+            }}
+            disabled={!hasNextPageGroup}
+          >
+            <ChevronRightIcon className="pb-0.5 text-gray-400" />
+          </button>
+        </div>
       </div>
     </div>
   );
