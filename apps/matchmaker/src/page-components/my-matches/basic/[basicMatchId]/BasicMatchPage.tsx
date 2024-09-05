5ba607ba96033ade79256b9b7dca044697777933
jaehoo <im.jaehoo1@gmail.com>
2024-09-05 13:54:41 +0900

    update

--- apps/matchmaker/src/page-components/my-matches/basic/[basicMatchId]/BasicMatchPage.tsx
+++ apps/matchmaker/src/page-components/my-matches/basic/[basicMatchId]/BasicMatchPage.tsx
@@ -13,18 +13,7 @@ import { useSlackNotibot } from "~/hooks/useSlackNotibot";
 import { useMemberAuthContext } from "~/providers/MemberAuthProvider";
 import { api } from "~/utils/api";
 import { formatUniqueMemberName } from "~/utils/formatUniqueMemberName";
-
-function KakaoBrowserFallback() {
-  return (
-    <div className="flex w-full items-start gap-1">
-      <p className="text-md text-gray-800">※</p>
-      <p className="text-md text-gray-800">
-        개인정보 유출 방지 정책으로 상대방 프로필은 카카오톡 브라우저에서만 조회
-        가능합니다.
-      </p>
-    </div>
-  );
-}
+import { KakaoBrowserFallback } from "../../components/KakaoBrowserFallback";
 
 export function BasicMatchPage() {
   return (
---
+++ apps/matchmaker/src/page-components/my-matches/components/KakaoBrowserFallback.tsx
@@ -0,0 +1,11 @@
+export function KakaoBrowserFallback() {
+  return (
+    <div className="flex w-full items-start gap-1">
+      <p className="text-md text-gray-800">※</p>
+      <p className="text-md text-gray-800">
+        개인정보 유출 방지 정책으로 상대방 프로필은 카카오톡 브라우저에서만 조회
+        가능합니다.
+      </p>
+    </div>
+  );
+}
