04506002f6061bb498c7b797993677bcba1cbe9a
jaehoo <im.jaehoo1@gmail.com>
2024-09-12 21:46:45 +0900

    update

--- apps/matchmaker/src/page-components/my-matches/_components/megaphone/PastMegaphoneMatchesAsSenderSectionResolved.tsx
+++ apps/matchmaker/src/page-components/my-matches/_components/megaphone/PastMegaphoneMatchesAsSenderSectionResolved.tsx
@@ -11,13 +11,36 @@ export function PastMegaphoneMatchesAsSenderSectionResolved() {
 
   assert(member != null, "Component should be used within MemberAuthGuard");
 
-  const [pastMegaphoneMatchesAsSender] =
-    api.megaphoneMatchRouter.findPastMatchesAsSenderByMemberId.useSuspenseQuery(
+  const [pastRespondedMegaphoneMatchesAsSender] =
+    api.megaphoneMatchRouter.findRespondedPastMatchesAsSenderByMemberId.useSuspenseQuery(
+      { memberId: member.id },
+    );
+  const [pastRejectedMegaphoneMatchesAsSender] =
+    api.megaphoneMatchRouter.findRejectedPastMatchesAsSenderByMemberId.useSuspenseQuery(
+      { memberId: member.id },
+    );
+
+  return pastRespondedMegaphoneMatchesAsSender.length > 0 ||
+    pastRejectedMegaphoneMatchesAsSender.length > 0 ? (
+    <>
+      <RespondedResolved />
+      <RejectedResolved />
+    </>
+  ) : null;
+}
+
+function RespondedResolved() {
+  const { member } = useMemberAuthContext();
+
+  assert(member != null, "Component should be used within MemberAuthGuard");
+
+  const [pastRespondedMegaphoneMatchesAsSender] =
+    api.megaphoneMatchRouter.findRespondedPastMatchesAsSenderByMemberId.useSuspenseQuery(
       { memberId: member.id },
     );
   const { sendMessage } = useSlackNotibot();
 
-  return pastMegaphoneMatchesAsSender.length > 0 ? (
+  return pastRespondedMegaphoneMatchesAsSender.length > 0 ? (
     <div className="flex w-full flex-col gap-2">
       <h2 className="text-xl font-semibold text-gray-800">응답 완료</h2>
       <div className="flex w-full items-start gap-1 text-sm">
@@ -25,7 +48,7 @@ export function PastMegaphoneMatchesAsSenderSectionResolved() {
         <p className="text-gray-500">성사된 프로필만 다시 볼 수 있습니다.</p>
       </div>
       <div className="grid grid-cols-2 gap-3">
-        {pastMegaphoneMatchesAsSender.map((match) => {
+        {pastRespondedMegaphoneMatchesAsSender.map((match) => {
           return (
             <MegaphoneMatchAsSenderCard
               key={match.id}
@@ -40,6 +63,36 @@ export function PastMegaphoneMatchesAsSenderSectionResolved() {
                 });
               }}
               disabled={match.status !== MatchStatus.ACCEPTED}
+            />
+          );
+        })}
+      </div>
+    </div>
+  ) : null;
+}
+
+function RejectedResolved() {
+  const { member } = useMemberAuthContext();
+
+  assert(member != null, "Component should be used within MemberAuthGuard");
+
+  const [pastRejectedMegaphoneMatchesAsSender] =
+    api.megaphoneMatchRouter.findRejectedPastMatchesAsSenderByMemberId.useSuspenseQuery(
+      { memberId: member.id },
+    );
+
+  return pastRejectedMegaphoneMatchesAsSender.length > 0 ? (
+    <div className="flex w-full flex-col gap-2">
+      <h2 className="text-xl font-semibold text-gray-800">상대방 거절</h2>
+      <div className="grid grid-cols-2 gap-3">
+        {pastRejectedMegaphoneMatchesAsSender.map((match) => {
+          return (
+            <MegaphoneMatchAsSenderCard
+              key={match.id}
+              match={match}
+              showRemainingHours={false}
+              showLabel={true}
+              disabled={false}
             />
           );
         })}
