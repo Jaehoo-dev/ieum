17d6677bfda7153753e771e54566767e991fe993
jaehoo <im.jaehoo1@gmail.com>
2024-09-18 14:35:13 +0900

    excludedIds

--- packages/blind-trpc-server/src/routers/blindMember.ts
+++ packages/blind-trpc-server/src/routers/blindMember.ts
@@ -36,36 +36,46 @@ export const blindMemberRouter = createTRPCRouter({
         gender: z.nativeEnum(Gender),
         take: z.number().min(1).max(100).default(20),
         cursor: z.string().optional(),
+        excludedIds: z.array(z.string()).optional(),
       }),
     )
-    .query(async ({ ctx: { prisma }, input: { gender, take, cursor } }) => {
-      const members = await prisma.blindMember.findMany({
-        where: {
-          gender,
-          status: MemberStatus.ACTIVE,
-        },
-        take: take + 1,
-        cursor: cursor ? { id: cursor } : undefined,
-        select: {
-          id: true,
-          nickname: true,
-          birthYear: true,
-          gender: true,
-          height: true,
-          bodyShape: true,
-          job: true,
-          residence: true,
-        },
-        orderBy: {
-          birthYear: "asc",
-        },
-      });
+    .query(
+      async ({
+        ctx: { prisma },
+        input: { gender, take, cursor, excludedIds },
+      }) => {
+        const members = await prisma.blindMember.findMany({
+          where: {
+            gender,
+            status: MemberStatus.ACTIVE,
+            id: {
+              notIn: excludedIds,
+            },
+          },
+          take: take + 1,
+          cursor: cursor ? { id: cursor } : undefined,
+          select: {
+            id: true,
+            nickname: true,
+            birthYear: true,
+            gender: true,
+            height: true,
+            bodyShape: true,
+            job: true,
+            residence: true,
+          },
+          orderBy: {
+            birthYear: "asc",
+          },
+        });
 
-      const nextCursor = members.length > take ? members.pop()!.id : undefined;
+        const nextCursor =
+          members.length > take ? members.pop()!.id : undefined;
 
-      return {
-        members,
-        nextCursor,
-      };
-    }),
+        return {
+          members,
+          nextCursor,
+        };
+      },
+    ),
 });
