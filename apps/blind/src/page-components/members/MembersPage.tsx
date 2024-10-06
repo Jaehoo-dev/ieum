5378b267940893ede81e571014c6325a79bff128
jaehoo <im.jaehoo1@gmail.com>
2024-10-06 23:21:17 +0900

    update

--- apps/blind/src/page-components/members/MembersPage.tsx
+++ apps/blind/src/page-components/members/MembersPage.tsx
@@ -140,7 +140,7 @@ function FetchMoreButton({
 
 MembersPage.getLayout = function getLayout(page: ReactElement) {
   return (
-    <Layout title="회원 목록" width="lg" bottomNav={true}>
+    <Layout title="이성 회원 목록" width="lg" bottomNav={true}>
       {page}
     </Layout>
   );
