e5666f23c0b009ab8f863dcf021ee3469da5857b
jaehoo <im.jaehoo1@gmail.com>
2024-10-06 23:27:26 +0900

    my profile page

---
+++ apps/blind/src/page-components/my-profile/MyProfilePage.tsx
@@ -0,0 +1,38 @@
+import { ReactElement, Suspense } from "react";
+import { BlindProfile } from "@ieum/profile";
+import { assert } from "@ieum/utils";
+
+import { Layout } from "~/components/Layout";
+import { Loader } from "~/components/Loader";
+import { useMemberAuthContext } from "~/providers/MemberAuthProvider";
+import { api } from "~/utils/api";
+
+export function MyProfilePage() {
+  return (
+    <Suspense
+      fallback={
+        <div className="mt-4 flex items-center justify-center">
+          <Loader />
+        </div>
+      }
+    >
+      <Resolved />
+    </Suspense>
+  );
+}
+
+function Resolved() {
+  const { member } = useMemberAuthContext();
+
+  assert(member != null, "Component should be used within MemberAuthGuard");
+
+  const [profile] = api.blindMemberRouter.getProfile.useSuspenseQuery({
+    memberId: member.id,
+  });
+
+  return <BlindProfile profile={profile} />;
+}
+
+MyProfilePage.getLayout = function getLayout(page: ReactElement) {
+  return <Layout title="내 프로필">{page}</Layout>;
+};
