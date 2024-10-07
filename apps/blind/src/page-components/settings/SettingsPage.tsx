a5a5e2a14b827dd5d618ea074039fb215bb9997b
jaehoo <im.jaehoo1@gmail.com>
2024-10-07 14:46:33 +0900

    update

--- apps/blind/src/page-components/settings/SettingsPage.tsx
+++ apps/blind/src/page-components/settings/SettingsPage.tsx
@@ -21,7 +21,7 @@ export function SettingsPage() {
 
 function Resolved() {
   return (
-    <div className="flex flex-col gap-6">
+    <div className="mb-6 flex flex-col gap-6">
       <StatusSectionResolved />
       <hr />
       <VerificationSection />
