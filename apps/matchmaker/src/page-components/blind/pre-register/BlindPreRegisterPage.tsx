5443348a8cf2a61c77d5b48b8f2b04e25a6625b5
jaehoo <im.jaehoo1@gmail.com>
2024-10-10 18:09:55 +0900

    kakaotalk id

--- apps/matchmaker/src/page-components/blind/pre-register/BlindPreRegisterPage.tsx
+++ apps/matchmaker/src/page-components/blind/pre-register/BlindPreRegisterPage.tsx
@@ -4,7 +4,7 @@ import { assert, formatUniqueMemberName } from "@ieum/utils";
 import { Layout } from "~/components/Layout";
 import { useSlackNotibot } from "~/hooks/useSlackNotibot";
 import { useMemberAuthContext } from "~/providers/MemberAuthProvider";
-import { ResolvedPreRegisterButton } from "./_components/ResolvedPreRegisterButton";
+import { ResolvedPreRegisterSection } from "./_components/ResolvedPreRegisterButton";
 
 export function BlindPreRegisterPage() {
   const { member } = useMemberAuthContext();
@@ -22,57 +22,51 @@ export function BlindPreRegisterPage() {
   }, []);
 
   return (
-    <>
-      <div className="mb-24 flex flex-col gap-10 text-gray-700">
-        <div className="flex flex-col gap-4">
-          <h2 className="border-b border-blind-500 pb-2 text-lg font-semibold text-blind-500">
-            이음 블라인드란?
-          </h2>
-          <div className="flex flex-col gap-2">
-            <Description
-              icon="🎊"
-              text="이음 블라인드는 기존 이음(이음 베이직)과 별개로 운영하는 새로운 서비스예요."
-            />
-            <Description
-              icon="🔍"
-              text="이음 매니저의 제안을 기다리지 않고 직접 짝을 찾습니다."
-            />
-            <Description
-              icon="📃"
-              text="회원 목록에서 간략 프로필(출생연도, 지역, 키, 체형, 직업, 자기소개)을 보고 이성에게 호감을 보내요."
-            />
-            <Description
-              icon="💖"
-              text="다른 회원도 나의 프로필을 보고 호감을 보낼 수 있어요."
-            />
-            <Description
-              icon="📷"
-              text="간략 프로필엔 사진을 공개하지 않아요. 사진을 주고받을지는 성사 이후 당사자끼리 합의합니다."
-            />
-            <Description
-              icon="💸"
-              text="기존 회원님들껜 가입비를 면제해드려요. 호감 표시와 성사 비용도 무료로 시작하려고 해요."
-            />
-          </div>
-        </div>
-        <div className="flex flex-col gap-4">
-          <h3 className="border-b border-blind-500 pb-2 text-lg font-semibold text-blind-500">
-            안내사항
-          </h3>
-          <div className="flex flex-col gap-2">
-            <Disclaimer text="이음 블라인드는 이음 베이직과 별도 회원 체계로 운영해요. 따라서 매칭이 겹칠 수도 있어요." />
-            <Disclaimer text="추후 유료화할 수 있어요. 미리 고지할 테니 걱정마세요!" />
-          </div>
+    <div className="mb-10 flex flex-col gap-10 text-gray-700">
+      <div className="flex flex-col gap-4">
+        <h2 className="border-b border-blind-500 pb-2 text-lg font-semibold text-blind-500">
+          이음 블라인드란?
+        </h2>
+        <div className="flex flex-col gap-2">
+          <Description
+            icon="🎊"
+            text="이음 블라인드는 기존 이음(이음 베이직)과 별개로 운영하는 새로운 서비스예요."
+          />
+          <Description
+            icon="🔍"
+            text="이음 매니저의 제안을 기다리지 않고 직접 짝을 찾습니다."
+          />
+          <Description
+            icon="📃"
+            text="회원 목록에서 간략 프로필(출생연도, 지역, 키, 체형, 직업, 자기소개)을 보고 이성에게 호감을 보내요."
+          />
+          <Description
+            icon="💖"
+            text="다른 회원도 나의 프로필을 보고 호감을 보낼 수 있어요."
+          />
+          <Description
+            icon="📷"
+            text="간략 프로필엔 사진을 공개하지 않아요. 사진을 주고받을지는 성사 이후 당사자끼리 합의합니다."
+          />
+          <Description
+            icon="💸"
+            text="기존 회원님들껜 가입비를 면제해드려요. 호감 표시와 성사 비용도 무료로 시작하려고 해요."
+          />
         </div>
       </div>
-      <div className="fixed bottom-0 left-0 flex w-full items-center justify-center border-t border-gray-200 bg-white p-4 md:px-6">
-        <div className="w-full max-w-lg px-2">
-          <Suspense fallback={<ResolvedPreRegisterButton.Skeleton />}>
-            <ResolvedPreRegisterButton />
-          </Suspense>
+      <div className="flex flex-col gap-4">
+        <h3 className="border-b border-blind-500 pb-2 text-lg font-semibold text-blind-500">
+          안내사항
+        </h3>
+        <div className="flex flex-col gap-2">
+          <Disclaimer text="이음 블라인드는 이음 베이직과 별도 회원 체계로 운영해요. 따라서 매칭이 겹칠 수도 있어요." />
+          <Disclaimer text="추후 유료화할 수 있어요. 미리 고지할 테니 걱정마세요!" />
         </div>
       </div>
-    </>
+      <Suspense>
+        <ResolvedPreRegisterSection />
+      </Suspense>
+    </div>
   );
 }
 
--- packages/matchmaker-trpc-server/src/routers/blind.ts
+++ packages/matchmaker-trpc-server/src/routers/blind.ts
@@ -5,13 +5,50 @@ import { z } from "zod";
 import { createTRPCRouter, protectedMatchmakerProcedure } from "../trpc";
 
 export const blindRouter = createTRPCRouter({
+  updateKakaotalkId: protectedMatchmakerProcedure
+    .input(
+      z.object({
+        memberId: z.string(),
+        kakaotalkId: z.string(),
+      }),
+    )
+    .mutation(async ({ ctx: { prisma }, input: { memberId, kakaotalkId } }) => {
+      const member = await prisma.basicMemberV2.findUniqueOrThrow({
+        where: {
+          id: memberId,
+        },
+        select: {
+          name: true,
+          phoneNumber: true,
+        },
+      });
+
+      await prisma.blindPreRegisteredMember.update({
+        where: {
+          phoneNumber: member.phoneNumber,
+        },
+        data: {
+          kakaotalkId,
+        },
+      });
+
+      sendSlackMessage({
+        channel: "폼_제출_알림",
+        content: `${formatUniqueMemberName(
+          member,
+        )} - 이음 블라인드 카카오톡 ID 업데이트`,
+      });
+
+      return true;
+    }),
   preRegister: protectedMatchmakerProcedure
     .input(
       z.object({
         memberId: z.string(),
+        kakaotalkId: z.string(),
       }),
     )
-    .mutation(async ({ ctx: { prisma }, input: { memberId } }) => {
+    .mutation(async ({ ctx: { prisma }, input: { memberId, kakaotalkId } }) => {
       const member = await prisma.basicMemberV2.findUniqueOrThrow({
         where: {
           id: memberId,
@@ -26,6 +63,7 @@ export const blindRouter = createTRPCRouter({
         data: {
           name: member.name,
           phoneNumber: member.phoneNumber,
+          kakaotalkId,
         },
       });
 
@@ -36,32 +74,44 @@ export const blindRouter = createTRPCRouter({
 
       return true;
     }),
-  hasPreRegistered: protectedMatchmakerProcedure
+  getPreRegisterStatus: protectedMatchmakerProcedure
     .input(
       z.object({
         memberId: z.string(),
       }),
     )
-    .query(async ({ ctx: { prisma }, input: { memberId } }) => {
-      const member = await prisma.basicMemberV2.findUniqueOrThrow({
-        where: {
-          id: memberId,
-        },
-        select: {
-          phoneNumber: true,
-        },
-      });
-
-      const preRegisteredMember =
-        await prisma.blindPreRegisteredMember.findUnique({
+    .query(
+      async ({
+        ctx: { prisma },
+        input: { memberId },
+      }): Promise<"DONE" | "KAKAOTALK_ID_MISSING" | "NON_REGISTERED"> => {
+        const member = await prisma.basicMemberV2.findUniqueOrThrow({
           where: {
-            phoneNumber: member.phoneNumber,
+            id: memberId,
           },
           select: {
-            id: true,
+            phoneNumber: true,
           },
         });
 
-      return preRegisteredMember != null;
-    }),
+        const preRegisteredMember =
+          await prisma.blindPreRegisteredMember.findUnique({
+            where: {
+              phoneNumber: member.phoneNumber,
+            },
+            select: {
+              id: true,
+              kakaotalkId: true,
+            },
+          });
+
+        if (preRegisteredMember == null) {
+          return "NON_REGISTERED";
+        }
+
+        return preRegisteredMember.kakaotalkId == null
+          ? "KAKAOTALK_ID_MISSING"
+          : "DONE";
+      },
+    ),
 });
