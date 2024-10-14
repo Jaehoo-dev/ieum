abd958a066fab528b72b180097de7f11d5aeb40f
jaehoo <im.jaehoo1@gmail.com>
2024-10-14 12:24:16 +0900

    update

--- apps/blind/src/page-components/members/MembersPage.tsx
+++ apps/blind/src/page-components/members/MembersPage.tsx
@@ -169,7 +169,7 @@ function MembersTable({
           <div className="font-medium text-gray-800">체형</div>
           <div className="font-medium text-gray-800">직업</div>
         </div>
-        <div className="max-h-[calc(100vh-220px)] divide-y overflow-y-auto overflow-x-hidden">
+        <div className="max-h-[calc(100vh-300px)] divide-y overflow-y-auto overflow-x-hidden">
           {members.map((member, index) => {
             const 동성인가 = self.gender === member.gender;
 
