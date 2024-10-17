fec1f3bb3cd8fec82e67b4a4d42e9c94c623d15a
jaehoo <im.jaehoo1@gmail.com>
2024-10-18 02:05:13 +0900

    주석 처리 제거

--- apps/admin/src/pages/api/schedule.ts
+++ apps/admin/src/pages/api/schedule.ts
@@ -17,67 +17,67 @@ export default async function handler(
     return res.status(401).json({ success: false });
   }
 
-  // const lastNotification = await prisma.blindBulkNotification.findFirst({
-  //   orderBy: {
-  //     sentAt: "desc",
-  //   },
-  // });
+  const lastNotification = await prisma.blindBulkNotification.findFirst({
+    orderBy: {
+      sentAt: "desc",
+    },
+  });
 
-  // assert(lastNotification != null, "No previous notification found");
+  assert(lastNotification != null, "No previous notification found");
 
-  // const matchesCreatedAfterLastNotification = await prisma.blindMatch.findMany({
-  //   where: {
-  //     createdAt: {
-  //       gt: lastNotification.sentAt,
-  //     },
-  //     status: BlindMatchStatus.PENDING,
-  //   },
-  //   include: {
-  //     respondent: {
-  //       select: {
-  //         phoneNumber: true,
-  //       },
-  //     },
-  //   },
-  // });
+  const matchesCreatedAfterLastNotification = await prisma.blindMatch.findMany({
+    where: {
+      createdAt: {
+        gt: lastNotification.sentAt,
+      },
+      status: BlindMatchStatus.PENDING,
+    },
+    include: {
+      respondent: {
+        select: {
+          phoneNumber: true,
+        },
+      },
+    },
+  });
 
-  // const respondentPhoneNumbers = Array.from(
-  //   new Set(
-  //     matchesCreatedAfterLastNotification
-  //       .map((match) => {
-  //         return match.respondent.phoneNumber;
-  //       })
-  //       .filter((phoneNumber) => {
-  //         return isKrPhoneNumberWithoutHyphens(phoneNumber);
-  //       }),
-  //   ),
-  // );
+  const respondentPhoneNumbers = Array.from(
+    new Set(
+      matchesCreatedAfterLastNotification
+        .map((match) => {
+          return match.respondent.phoneNumber;
+        })
+        .filter((phoneNumber) => {
+          return isKrPhoneNumberWithoutHyphens(phoneNumber);
+        }),
+    ),
+  );
 
-  // await Promise.all([
-  //   solapiMessageService.send(
-  //     respondentPhoneNumbers.map((phoneNumber) => {
-  //       return {
-  //         to: phoneNumber,
-  //         from: process.env.ADMIN_PHONE_NUMBER,
-  //         text: `[이음 블라인드] 하트가 도착했습니다! '매칭 목록' 탭에서 확인해보세요.
-  // ${IEUM_BLIND_MATCHES_PAGE_URL}`,
-  //       };
-  //     }),
-  //   ),
-  //   prisma.blindBulkNotification.create({
-  //     data: {
-  //       sentAt: new Date(),
-  //       sentTo: respondentPhoneNumbers,
-  //     },
-  //   }),
-  //   prisma.otp.deleteMany({
-  //     where: {
-  //       expiresAt: {
-  //         lt: new Date(),
-  //       },
-  //     },
-  //   }),
-  // ]);
+  await Promise.all([
+    solapiMessageService.send(
+      respondentPhoneNumbers.map((phoneNumber) => {
+        return {
+          to: phoneNumber,
+          from: process.env.ADMIN_PHONE_NUMBER,
+          text: `[이음 블라인드] 하트가 도착했습니다! '매칭 목록' 탭에서 확인해보세요.
+  ${IEUM_BLIND_MATCHES_PAGE_URL}`,
+        };
+      }),
+    ),
+    prisma.blindBulkNotification.create({
+      data: {
+        sentAt: new Date(),
+        sentTo: respondentPhoneNumbers,
+      },
+    }),
+    prisma.otp.deleteMany({
+      where: {
+        expiresAt: {
+          lt: new Date(),
+        },
+      },
+    }),
+  ]);
 
   res.status(200).json({ success: true });
 }
