5a36f8fd22d15ec80228105fc98ef5664202b639
jaehoo <im.jaehoo1@gmail.com>
2024-11-18 12:00:58 +0900

    log

--- apps/admin/src/pages/api/reset-hearts.ts
+++ apps/admin/src/pages/api/reset-hearts.ts
@@ -21,5 +21,10 @@ export default async function handler(
     },
   });
 
+  await sendSlackMessage({
+    channel: "알림",
+    content: `하트 초기화 완료 ${SLACK_MANAGER1_ID_MENTION}`,
+  });
+
   res.status(200).json({ success: true });
 }
